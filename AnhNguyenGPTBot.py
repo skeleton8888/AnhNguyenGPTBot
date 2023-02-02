0c715f89fdfef0cfa05de7e5116ed3b07b309a2b
skeleton8888 <qnanh88@Gmail.com>
2023-02-02 13:49:49 +0700

    init commit

--- AnhNguyenGPTBot.py
+++ AnhNguyenGPTBot.py
@@ -1,53 +1,26 @@
-import os
 import openai
-import logging
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters
 
-openai.api_key = "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"
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-def chat(update, context):
-    """Echo the user message."""
+def response(update, context):
+    message = update.message.text
     response = openai.Completion.create(
-      engine="text-davinci-002",
-      prompt="User: " + update.message.text + "\nBot: ",
-      max_tokens=1024,
-      n=1,
-      stop=None,
-      temperature=0.5,
+        engine="text-davinci-002",
+        prompt=message,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
     ).choices[0].text
-    update.message.reply_text(response)
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True)
-
-    # Get the dispatcher to register handlers
+    token = "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"
+    updater = Updater(token, use_context=True)
     dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, response))
+    updater.start_polling()
+    updater.idle()
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(Message
+if __name__ == '__main__':
+    main()
